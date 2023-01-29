c5390522b29355424e79840be6a8ea7467bfeecc
Anton Belov <anton.belov.al7@gmail.com>
2023-01-29 11:08:16 +0000

    first

---
+++ pages/api/post/[id].ts
@@ -0,0 +1,19 @@
+// pages/api/post/[id].ts
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../lib/prisma';
+
+// DELETE /api/post/:id
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+    const postId = req.query.id.toString();
+    if (req.method === 'DELETE') {
+        const post = await prisma.post.delete({
+        where: { id: postId },
+        });
+        res.json(post);
+    } else {
+        throw new Error(
+        `The HTTP ${req.method} method is not supported at this route.`,
+        );
+    }
+}
